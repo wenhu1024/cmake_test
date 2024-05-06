61e0aece4051d1c22e6d7b4b58fb69a4a3ff9f9e
wenhu1024 <1340773297@qq.com>
2024-05-06 16:03:06 +0800

    04

---
+++ ch02/recipe_04/build/CMakeFiles/arch-dependent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arch-dependent.dir/arch-dependent.cpp.o"
+  "CMakeFiles/arch-dependent.dir/arch-dependent.cpp.o.d"
+  "arch-dependent"
+  "arch-dependent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arch-dependent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
