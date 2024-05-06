128172c0e38acdef75c45cf770b77febbf41c4d6
wenhu1024 <1340773297@qq.com>
2024-05-06 10:53:40 +0800

    0

---
+++ ch01/recipe_09/build/CMakeFiles/animals.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wenhu/cmake_test/ch01/recipe_09/Animal.cpp" "CMakeFiles/animals.dir/Animal.cpp.o" "gcc" "CMakeFiles/animals.dir/Animal.cpp.o.d"
+  "/home/wenhu/cmake_test/ch01/recipe_09/Cat.cpp" "CMakeFiles/animals.dir/Cat.cpp.o" "gcc" "CMakeFiles/animals.dir/Cat.cpp.o.d"
+  "/home/wenhu/cmake_test/ch01/recipe_09/Dog.cpp" "CMakeFiles/animals.dir/Dog.cpp.o" "gcc" "CMakeFiles/animals.dir/Dog.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
