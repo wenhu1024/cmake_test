6788c241b33b22f464eda05f95770acb5acb866b
wenhu1024 <1340773297@qq.com>
2024-05-06 10:23:01 +0800

    08

---
+++ ch01/recipe_08/build/CMakeFiles/geometry.dir/geometry_rhombus.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/geometry.dir/geometry_rhombus.cpp.o: \
+ /home/wenhu/cmake_test/ch01/recipe_08/geometry_rhombus.cpp \
+ /usr/include/stdc-predef.h \
+ /home/wenhu/cmake_test/ch01/recipe_08/geometry_rhombus.hpp
