27a4f0e605e15e9caa8bcbc8f12c92699b1f4e7b
wenhu1024 <1340773297@qq.com>
2024-05-06 17:38:16 +0800

    05

---
+++ ch02/recipe_05/build/CMakeFiles/processor-info.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processor-info.dir/processor-info.cpp.o"
+  "CMakeFiles/processor-info.dir/processor-info.cpp.o.d"
+  "processor-info"
+  "processor-info.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/processor-info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch02/recipe_05/build/config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define NUMBER_OF_LOGICAL_CORES   16
+#define NUMBER_OF_PHYSICAL_CORES  8
+#define TOTAL_VIRTUAL_MEMORY      2048
+#define AVAILABLE_VIRTUAL_MEMORY  2048
+#define TOTAL_PHYSICAL_MEMORY     6866
+#define AVAILABLE_PHYSICAL_MEMORY 4725
+#define IS_64BIT                  1
+#define HAS_FPU                   1
+#define HAS_MMX                   1
+#define HAS_MMX_PLUS              0
+#define HAS_SSE                   1
+#define HAS_SSE2                  1
+#define HAS_SSE_FP                0
+#define HAS_SSE_MMX               0
+#define HAS_AMD_3DNOW             0
+#define HAS_AMD_3DNOW_PLUS        0
+#define HAS_IA64                  0
+#define OS_NAME                  "Linux"
+#define OS_RELEASE               "5.15.133.1-microsoft-standard-WSL2"
+#define OS_VERSION               "#1 SMP Thu Oct 5 21:02:42 UTC 2023"
+#define OS_PLATFORM              "x86_64"
