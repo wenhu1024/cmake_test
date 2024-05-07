4dfbe30efce69ff203ffcff708f028d80359ba7f
wenhu <1340773297@qq.com>
2024-05-07 10:38:16 +0800

    02

---
+++ ch03/recipe_02/build/CMakeFiles/hello-embedded-python.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello-embedded-python.dir/hello-embedded-python.c.o"
+  "CMakeFiles/hello-embedded-python.dir/hello-embedded-python.c.o.d"
+  "hello-embedded-python"
+  "hello-embedded-python.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello-embedded-python.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
